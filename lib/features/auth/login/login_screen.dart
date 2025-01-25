a2d7c7df596797e3186abb670e2da790e0ca401f
Ahmed Abdelaziz <ahmed.abdelaziz3080@gmail.com>
2025-01-25 14:39:17 +0200

    init branch development

--- lib/features/auth/login/login_screen.dart
+++ lib/features/auth/login/login_screen.dart
@@ -7,9 +7,8 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
+    return   const Scaffold(
       body: Center(
-        child: Text(AppLocalKeys.arabic.tr()),
       ),
     );
   }
