022ea5cc73a6d5c95d224cc4b2561c6e9124f355
mohammed <mobiledev5122003@gmail.com>
2026-06-06 11:57:19 +0200

    fix: change ndkVersion to highest complete installed version 29.0.13599879

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.thecoreapps.team_ar"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "26.3.11579264"
+    ndkVersion = "29.0.13599879"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
