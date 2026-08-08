75fa5f97d00e507203669949fd9626010f2dad44
mohammed <mobiledev5122003@gmail.com>
2026-08-08 14:07:02 +0200

    build: force jvmTarget 17 for Kotlin compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,10 +12,19 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
+}
+
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
 }
